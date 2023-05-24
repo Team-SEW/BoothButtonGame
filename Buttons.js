aefd52b98bdcdd1ce6afd2a13bb973a159c98c97
Robert Teeling <60351835+teelingr@users.noreply.github.com>
2023-05-24 10:55:19 +0100

    Add files via upload

---
+++ Buttons.js
@@ -0,0 +1,58 @@
+const buttons = document.querySelectorAll('.button');
+const statusBox = document.getElementById('status-box');
+const statusText = document.getElementById('status-text');
+
+// Define an array of button texts that should trigger the box turning green
+const greenButtons = [
+  'Fault Code: F04',
+  'Extend deceleration ramps',
+  'Check feeder cable to braking resistor',
+  'Check technical data of braking resistor',
+  'Replace Movidrive'
+];
+
+// const greenButtons = [
+//     'Fault Code: F27',
+//     'Emergency Stop Pressed',
+//     'Open circuit/both limit switches missing.',
+//     'Limit switches are swapped over in relation to direction of rotation of motor.',
+//     'Check wiring of limit switches.',
+//     'Reprogram terminals'
+// ];   
+        
+// const greenButtons = [
+//     'Fault Code: F08',
+//     'Reduce load',
+//     'Increase deceleration time',
+//     'Check encoder connection',
+//     'Check current limitation',
+//     'Extend ramps if necessary',
+//     'Check motor cable and motor',
+//     'Check line phases'
+// ];  
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    button.classList.toggle('highlighted');
+
+    if (button.classList.contains('highlighted')) {
+      button.style.backgroundColor = 'red';
+    } else {
+      button.style.backgroundColor = '';
+    }
+
+    // Check if all green buttons are highlighted
+    const allGreenButtonsHighlighted = greenButtons.every(buttonText =>
+      Array.from(buttons).find(btn => btn.innerText === buttonText)?.classList.contains('highlighted')
+    );
+
+    // Change the background color of the status box
+    if (allGreenButtonsHighlighted) {
+      statusBox.style.backgroundColor = 'green';
+      statusText.textContent = 'Machine is Now Operational';
+    } else {
+      statusBox.style.backgroundColor = 'red';
+      statusText.textContent = 'System Error Machine is Inoperational';
+    }
+  });
+});
